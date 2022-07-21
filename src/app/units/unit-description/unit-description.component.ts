858fcf2748e7ba44e0700ca8b342aa9485e56b27
Radkillua <radwahmhus@gmail.com>
2022-07-22 00:43:45 +0200

    unit-description Module

---
+++ src/app/units/unit-description/unit-description.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-unit-description',
+  templateUrl: './unit-description.component.html',
+  styleUrls: ['./unit-description.component.css']
+})
+export class UnitDescriptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

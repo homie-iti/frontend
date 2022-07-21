171fb358051f0354ee722422ae35723b007907cd
Radkillua <radwahmhus@gmail.com>
2022-07-22 00:15:25 +0200

    removing Login and signUp

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SignUpComponent } from './sign-up/sign-up.component';
-import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
   declarations: [
-    SignUpComponent,
-    LoginComponent
+
   ],
   imports: [
     CommonModule

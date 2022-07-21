f8a3bc3c5fa6dbeff6e089a9c5e3a8a0b96ff87a
Radkillua <radwahmhus@gmail.com>
2022-07-22 00:45:15 +0200

    unit-review module

---
+++ src/app/units/unit-review/unit-review.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-unit-review',
+  templateUrl: './unit-review.component.html',
+  styleUrls: ['./unit-review.component.css']
+})
+export class UnitReviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

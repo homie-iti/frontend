74b0e91c6bd90e4f070927a383364f7b20daa239
EngMostafaHussien <108274816+EngMostafaHussien@users.noreply.github.com>
2022-07-22 20:21:29 +0200

    unit info component

--- src/app/units/unit-info/unit-info.component.ts
+++ src/app/units/unit-info/unit-info.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {faToilet , faKitchenSet,faShower,faParking,faCouch,faTemperatureArrowUp , faTree,faWifi,faBed,faElevator,faWheelchair,faHouseLock,faTv,faMusic,faSmoking,faPaw} from "@fortawesome/free-solid-svg-icons"
+
 
 @Component({
   selector: 'app-unit-info',
@@ -7,6 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UnitInfoComponent implements OnInit {
 
+  faToilet=faToilet;
+  faKitchenSet=faKitchenSet;
+  faShower=faShower;
+  faParking=faParking;
+  faCouch=faCouch;
+  faTemperatureArrowUp=faTemperatureArrowUp;
+  faTree=faTree;
+  faWifi=faWifi;
+  faBed=faBed;
+  faElevator=faElevator;
+  faTv=faTv;
+  faHouseLock=faHouseLock;
+  faWheelchair=faWheelchair;
+  faMusic=faMusic;
+  faSmoking=faSmoking;
+  faPaw=faPaw;
   constructor() { }
 
   ngOnInit(): void {

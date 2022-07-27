b94ed1e381af5c726dc666b55feccfd7888603af
EngMostafaHussien <108274816+EngMostafaHussien@users.noreply.github.com>
2022-07-27 05:17:06 +0200

    edit folders

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.page';
-import { UnitPageComponent } from './units/unit.page/unit.page';
+import { UnitPageComponent } from './units/unit page/unit.page/unit.page';
 import { UnitsComponent } from './units/units.page';
-import { LoginComponent } from './user/login/login.component';
-import { UserProfileComponent } from './user/user-profile/user-profile.component';
+import { LoginComponent } from './user/user registration page/login/login.component';
+import { UserProfileComponent } from './user/user account/user-profile/user-profile.component';
 // import { UserProfileComponent } from './user/user-profile/user-profile.component';
 import { UserComponent } from './user/user.page';
 
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UserCardComponent } from './user-card/user-card.component';
-import { UserAboutComponent } from './user-about/user-about.component';
-import { UserUnitListingComponent } from './user-unit-listing/user-unit-listing.component';
-import { LoginComponent } from './login/login.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
+import { UserCardComponent } from './user profile page/user-card/user-card.component';
+import { UserAboutComponent } from './user profile page/user-about/user-about.component';
+import { UserUnitListingComponent } from './user profile page/user-unit-listing/user-unit-listing.component';
+import { LoginComponent } from './user registration page/login/login.component';
+import { SignUpComponent } from './user registration page/sign-up/sign-up.component';
+import { UserProfileComponent } from './user account/user-profile/user-profile.component';
 import { UserComponent } from './user.page';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 

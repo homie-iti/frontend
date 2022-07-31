d6cfcd59e02eb51a6f5cde550b914c2ef0cf46de
safia elnakhal <safiaelnakhal@gmail.com>
2022-07-31 21:53:11 +0200

    update navbar & unitPage & user page & footer

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import {faHouseLaptop} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
-
+ 
+  faHouseLaptop = faHouseLaptop;
   constructor() { }
 
   ngOnInit(): void {

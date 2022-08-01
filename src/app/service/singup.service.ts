79d9e769b8e170599ccb13e6902a25132e75f312
Radkillua <radwahmhus@gmail.com>
2022-08-01 07:41:23 +0200

    sign up service

--- src/app/service/singup.service.ts
+++ src/app/service/singup.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
 import { Checkmail } from '../_models/signUp/checkmail';
 import { ChecknationalId } from '../_models/signUp/checknational-id';
 import { Checkphone } from '../_models/signUp/checkphone';
@@ -10,11 +11,17 @@ import { SignUpData } from '../_models/signUp/sign-up-data';
 })
 export class SingupService {
 
+
+
   private url: string = 'https://homie-iti.herokuapp.com/signup/check-availability';
   private Posturl:string='https://homie-iti.herokuapp.com/signup'
 
   checkAvailableEmail( checkAvailable: Checkmail) {
-    return this.http.post<any>(this.url, checkAvailable);
+      return this.http.post<any>(this.url, checkAvailable);
+
+    
+    
+    
   }
   
   checkAvailableNationlId( checkAvailable:ChecknationalId) {

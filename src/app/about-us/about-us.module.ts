5f74e3264bddf8477186f0dd40d9a0122cad19da
safia elnakhal <safiaelnakhal@gmail.com>
2022-08-03 02:30:53 +0200

    create about us page ==>create component  for all page

--- src/app/about-us/about-us.module.ts
+++ src/app/about-us/about-us.module.ts
@@ -1,19 +1,22 @@
+import { HomepageModule } from './../homepage/homepage.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutCoverComponent } from './about-cover/about-cover.component';
 import { AboutUsComponent } from './about-us.page'
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from '../app-routing.module';
+import { ValuesComponent } from './values/values.component';
 
 
 
 @NgModule({
   declarations: [
     AboutCoverComponent,
-    AboutUsComponent
+    AboutUsComponent,
+    ValuesComponent
   ],
   imports: [
-    CommonModule,RouterModule,AppRoutingModule
+    CommonModule,RouterModule,AppRoutingModule,HomepageModule
   ], exports: [
     AboutCoverComponent
   ]
---
+++ src/app/about-us/values/values.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ValuesComponent } from './values.component';
+
+describe('ValuesComponent', () => {
+  let component: ValuesComponent;
+  let fixture: ComponentFixture<ValuesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ValuesComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ValuesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

dc952d2c6c0477d2d8dc73bd9fc5a73e322dc9bf
Mahmoud Samy <mahmoudsamy50@outlook.com>
2022-08-04 21:38:16 +0200

    used reactive form in login and added validations

--- src/app/user/registration pages/login/login.component.ts
+++ src/app/user/registration pages/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../service/auth.service';
 import { Router } from '@angular/router';
+import { FormControl, FormGroup, FormRecord, Validators } from '@angular/forms';
+import { faChainSlash } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-login',
@@ -10,20 +12,42 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
   constructor(private authService: AuthService, private router: Router) {}
 
-  loginForm = {
-    email: '',
-    password: '',
-  };
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.pattern(
+        '"^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[@$!%*?&])[A-Za-zd@$!%*?&]{8,}$"'
+      ),
+    ]),
+  });
+
+  // {
+  //   email: '',
+  //   password: '',
+  // };
 
   doesLoginHasError = false;
 
   ngOnInit(): void {
     if (this.authService.isLoggedIn()) this.router.navigateByUrl('/home');
+
+    // this.loginForm = new FormGroup({
+    //   email: new FormControl(null),
+    //   password: new FormControl(null),
+    // });
   }
 
   login() {
+    console.log(
+      this.loginForm.get('email')?.value as string,
+      this.loginForm.get('password')?.value as string
+    );
     this.authService
-      .login(this.loginForm.email, this.loginForm.password)
+      .login(
+        this.loginForm.get('email')?.value as string,
+        this.loginForm.get('password')?.value as string
+      )
       .subscribe({
         next: (loginInfo) => {
           this.authService.setLoggedIn(true);

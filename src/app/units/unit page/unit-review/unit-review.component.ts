b61c1d19904d9dc890d13c3e143887e198aa878f
EngMostafaHussien <108274816+EngMostafaHussien@users.noreply.github.com>
2022-08-04 19:25:17 +0200

    edit review

--- src/app/units/unit page/unit-review/unit-review.component.ts
+++ src/app/units/unit page/unit-review/unit-review.component.ts
@@ -18,31 +18,28 @@ export class UnitReviewComponent implements OnInit {
   unitComment: any;
   agentData: any;
 
-  agentdetails:any
+  agentdetails: any;
 
   ngOnInit(): void {
     this.unitser
       .getUnitDetails(`/units/dc2eeacdf1dd3f9551fcbb3a`)
       .subscribe((a) => {
         this.unitComment = a.reviews.reviews;
-        for(let item in this.unitComment){
+        for (let item in this.unitComment) {
           if (typeof this.unitComment[item] === 'object') {
             for (const nestedKey in this.unitComment[item]) {
-              if (typeof this.unitComment[item][nestedKey] === 'object'){
+              if (typeof this.unitComment[item][nestedKey] === 'object') {
                 for (const nestedKey2 in this.unitComment[item][nestedKey]) {
-                  this.agentdetails=this.unitComment[item][nestedKey][nestedKey2]
+                  this.agentdetails =
+                    this.unitComment[item][nestedKey][nestedKey2];
+                }
               }
-              
-
             }
-
-        }}
-        console.log(this.agentdetails.fullName)
-        console.log(this.agentdetails.image)
-        // console.log(this.unitComment);
-      }});
-        // this.agentData = a.reviews.reviews[0].agentId;
-        // console.log(this.agentData);
-      // });
+          }
+          console.log(this.agentdetails.fullName);
+          console.log(this.agentdetails.image);
+          // console.log(this.unitComment);
+        }
+      });
   }
 }

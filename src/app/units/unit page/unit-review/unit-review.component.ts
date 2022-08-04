6467881bb453cd42e2772f882d21bd5dbaff3e00
EngMostafaHussien <108274816+EngMostafaHussien@users.noreply.github.com>
2022-08-04 18:34:00 +0200

    unit review

--- src/app/units/unit page/unit-review/unit-review.component.ts
+++ src/app/units/unit page/unit-review/unit-review.component.ts
@@ -1,18 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import {faStar} from "@fortawesome/free-solid-svg-icons"
-
+import { ActivatedRoute } from '@angular/router';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { GetdataService } from 'src/app/service/getdata.service';
 
 @Component({
   selector: 'app-unit-review',
   templateUrl: './unit-review.component.html',
-  styleUrls: ['./unit-review.component.css']
+  styleUrls: ['./unit-review.component.css'],
 })
 export class UnitReviewComponent implements OnInit {
-
-  faStar=faStar
-  constructor() { }
+  faStar = faStar;
+  constructor(
+    private activate: ActivatedRoute,
+    private unitser: GetdataService
+  ) {}
+  id: any = this.activate.snapshot.params['id'];
+  unitComment: any;
 
   ngOnInit(): void {
+    this.unitser
+      .getUnitDetails(`/units/dc2eeacdf1dd3f9551fcbb3a`)
+      .subscribe((a) => {
+        this.unitComment = a.reviews.reviews;
+        console.log(this.unitComment);
+      });
   }
-
 }

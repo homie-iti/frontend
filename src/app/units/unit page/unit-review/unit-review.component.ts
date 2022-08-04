9982880928f6ce7428a0711f10a19216b9246f24
Radkillua <radwahmhus@gmail.com>
2022-08-04 19:13:08 +0200

    nested array of objects to get agent details

--- src/app/units/unit page/unit-review/unit-review.component.ts
+++ src/app/units/unit page/unit-review/unit-review.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar, faThumbsDown } from '@fortawesome/free-solid-svg-icons';
 import { GetdataService } from 'src/app/service/getdata.service';
 
 @Component({
@@ -17,12 +17,28 @@ export class UnitReviewComponent implements OnInit {
   id: any = this.activate.snapshot.params['id'];
   unitComment: any;
 
+  agentdetails:any
+
   ngOnInit(): void {
     this.unitser
       .getUnitDetails(`/units/dc2eeacdf1dd3f9551fcbb3a`)
       .subscribe((a) => {
         this.unitComment = a.reviews.reviews;
-        console.log(this.unitComment);
-      });
+        for(let item in this.unitComment){
+          if (typeof this.unitComment[item] === 'object') {
+            for (const nestedKey in this.unitComment[item]) {
+              if (typeof this.unitComment[item][nestedKey] === 'object'){
+                for (const nestedKey2 in this.unitComment[item][nestedKey]) {
+                  this.agentdetails=this.unitComment[item][nestedKey][nestedKey2]
+              }
+              
+
+            }
+
+        }}
+        console.log(this.agentdetails.fullName)
+        console.log(this.agentdetails.image)
+        // console.log(this.unitComment);
+      }});
   }
 }

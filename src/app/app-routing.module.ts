c3f08bdd87b5a32f213d008950e3ba434f3ec494
Radkillua <radwahmhus@gmail.com>
2022-08-05 20:42:37 +0200

    remove land lord routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,6 @@ import { UserProfileComponent } from './user/user account/user-profile/user-prof
 // import { UserProfileComponent } from './user/user-profile/user-profile.component';
 import { UserComponent } from './user/user.page';
 import { SignUpComponent } from './user/registration pages/sign-up/sign-up.component';
-import { AddcityComponent } from './landlord/addcity/addcity.component';
 
 const routes: Routes = [
   {
@@ -56,10 +55,7 @@ const routes: Routes = [
     path: 'units/:id',
     component: UnitsComponent,
   },
-  {
-    path: 'addCity',
-    component: AddcityComponent,
-  },
+ 
   {
     path: '**',
     redirectTo: '',

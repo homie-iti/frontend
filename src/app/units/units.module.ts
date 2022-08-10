44726cebd483147843eefc2ab0ce926607ebe9a5
Mahmoud Samy <mahmoudsamy50@outlook.com>
2022-08-10 22:31:56 +0200

    added main payment files

--- src/app/units/units.module.ts
+++ src/app/units/units.module.ts
@@ -6,7 +6,6 @@ import { UnitCardComponent } from './All units page/unit-card/unit-card.componen
 
 // import { UnitPaginationComponent } from './All units page/unit-pagination/unit-pagination.component';
 
-
 import { UnitGalleryComponent } from './unit page/unit-gallery/unit-gallery.component';
 import { UnitDescriptionComponent } from './unit page/unit-description/unit-description.component';
 import { UnitInfoComponent } from './unit page/unit-info/unit-info.component';
@@ -21,6 +20,7 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from '../app-routing.module';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { NgxSpinnerModule } from 'ngx-spinner';
+import { PaymentInfoComponent } from './unit page/landlorddetails/payment-info/payment-info.component';
 
 @NgModule({
   declarations: [
@@ -30,7 +30,6 @@ import { NgxSpinnerModule } from 'ngx-spinner';
 
     // UnitPaginationComponent,
 
-
     UnitGalleryComponent,
     UnitDescriptionComponent,
     UnitInfoComponent,
@@ -39,6 +38,7 @@ import { NgxSpinnerModule } from 'ngx-spinner';
     UnitsComponent,
     UnitPageComponent,
     LandlorddetailsComponent,
+    PaymentInfoComponent,
   ],
   imports: [
     CommonModule,
@@ -55,7 +55,6 @@ import { NgxSpinnerModule } from 'ngx-spinner';
     UnitCardComponent,
 
     // UnitPaginationComponent,
-
 
     UnitGalleryComponent,
     UnitDescriptionComponent,

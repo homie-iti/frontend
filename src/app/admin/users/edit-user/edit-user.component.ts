dd230c814d4836ecd66dfa2aa2a34c78c6d680d5
maryamragab <mariam.relshafei@yahoo.com>
2022-08-10 15:32:32 +0200

    fix issues in update user

--- src/app/admin/users/edit-user/edit-user.component.ts
+++ src/app/admin/users/edit-user/edit-user.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AdminService } from 'src/app/service/admin.service';
+import { User } from 'src/app/_models/user';
 
 @Component({
   selector: 'app-edit-user',
@@ -16,28 +17,8 @@ export class EditUserComponent implements OnInit {
   ) {}
 
   id: string = this.route.snapshot.params['id'];
-  user: any = {};
-
-  updateUserForm = new FormGroup({
-    fullName: new FormControl('', [
-      Validators.required,
-      Validators.minLength(3),
-    ]),
-
-    email: new FormControl('', [Validators.required, Validators.email]),
-    age: new FormControl('', [Validators.required, Validators.min(18)]),
-    phone: new FormControl('', [Validators.required]),
-    gender: new FormControl('', [Validators.required]),
-    // address: new FormGroup({
-    //   city: new FormControl('', [Validators.required]),
-    //   streetName: new FormControl('', [Validators.required]),
-    //   buildingNumber: new FormControl('', [Validators.required]),
-    // }),
-    balance: new FormControl('', [Validators.required]),
-    isAgent: new FormControl(this.user.isAgent),
-    isLandlord: new FormControl(this.user.isLandlord),
-  });
-
+  user: User = {};
+  updateUserForm!: FormGroup;
   get userValues() {
     return this.updateUserForm.controls;
   }
@@ -49,13 +30,43 @@ export class EditUserComponent implements OnInit {
   updateUser(data: any) {
     this.adminService.updateUserData(this.id, data).subscribe((data) => {
       console.log(data);
-      this.router.navigateByUrl('all-users');
+      this.router.navigateByUrl('admin/all-users');
     });
   }
   ngOnInit(): void {
     this.adminService.getUserById(this.id).subscribe((userData) => {
       console.log(userData);
       this.user = userData;
+      this.updateUserForm = new FormGroup({
+        fullName: new FormControl(userData.fullName, [
+          Validators.required,
+          Validators.minLength(3),
+        ]),
+
+        email: new FormControl(userData.email, [
+          Validators.required,
+          Validators.email,
+        ]),
+        age: new FormControl(userData.age, [
+          Validators.required,
+          Validators.min(18),
+        ]),
+        phone: new FormControl(userData.phone, [Validators.required]),
+        gender: new FormControl(userData.gender, [Validators.required]),
+        // address: new FormGroup({
+        //   city: new FormControl(userData.address.city, [Validators.required]),
+        //   streetName: new FormControl(userData.address.streetName, [
+        //     Validators.required,
+        //   ]),
+        //   buildingNumber: new FormControl(userData.address.buildingNumber, [
+        //     Validators.required,
+        //   ]),
+        // }),
+        balance: new FormControl(userData.balance, [Validators.required]),
+        isAgent: new FormControl(userData.isAgent),
+        isLandlord: new FormControl(userData.isLandlord),
+      });
+      // this.updateUserForm.get("isAgent")?.setValue
     });
   }
 }

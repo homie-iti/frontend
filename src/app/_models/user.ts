8c2bc47d752e695e5ce5f2d9e1ab746888663a4f
maryamragab <mariam.relshafei@yahoo.com>
2022-08-10 11:47:13 +0200

    transfer users class data into user class

--- src/app/_models/user.ts
+++ src/app/_models/user.ts
@@ -1,1 +1,20 @@
-export interface User {}
+export class User {
+  constructor(
+    public _id?: string,
+    public fullName?: string,
+    public email?: string,
+    public password?: string,
+    public age?: number,
+    public gender?: string,
+    public phone?: string,
+    public national_id?: number,
+    public image?: string,
+    public balance?: number,
+    public address?: object,
+    public favoriteUnits?: string[],
+    public isLandlord?: boolean,
+    public isAgent?: boolean,
+    public isAccountActivated?: boolean,
+    public resetLink?: string
+  ) {}
+}

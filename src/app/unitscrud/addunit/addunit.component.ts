76bb9b683d123f5dbd530662d05c66eaf56d6618
Mahmoud Samy <mahmoudsamy50@outlook.com>
2022-08-11 22:51:10 +0200

    fixed adding unit issue

--- src/app/unitscrud/addunit/addunit.component.ts
+++ src/app/unitscrud/addunit/addunit.component.ts
@@ -4,115 +4,87 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/service/auth.service';
 import { GetdataService } from 'src/app/service/getdata.service';
 import { City } from 'src/app/_models/city';
-
-
+// import { AuthService } from '../../service/auth.service';
 
 @Component({
   selector: 'app-addunit',
   templateUrl: './addunit.component.html',
-  styleUrls: ['./addunit.component.css']
+  styleUrls: ['./addunit.component.css'],
 })
 export class AddunitComponent implements OnInit {
+  constructor(public data: GetdataService, public authService: AuthService) {}
 
-  constructor(public data:GetdataService, public auth:AuthService) { }
+  myform = new FormGroup({
+    unit: new FormGroup({
+      landlordId: new FormControl(' '),
+      cityId: new FormControl(' '),
+      estateType: new FormControl('', Validators.required),
+      numberOfResidents: new FormControl(0, Validators.required),
+      allowedGender: new FormControl('', Validators.required),
+      dailyPrice: new FormControl(0, Validators.required),
+      isAvailable: new FormControl(false, Validators.required),
+      isPetsAllowed: new FormControl(false, Validators.required),
 
-
-  myform=new FormGroup({
-    unit:new FormGroup({
-      landlordId : new FormControl( ' '),
-      cityId:new FormControl(' '),
-      estateType:new FormControl("",Validators.required),
-      numberOfResidents: new FormControl(0,Validators.required),
-      allowedGender:new FormControl("", Validators.required),
-      dailyPrice:new FormControl(0,Validators.required),
-      isAvailable:new FormControl(false,Validators.required),
-      isPetsAllowed:new FormControl(false,Validators.required),
-
-      unitInfo:new FormGroup({
-        description: new FormControl("", { validators:[
-          Validators.required, 
-          Validators.pattern("[a-zA-Z ]*")
-        ]}),
-        rooms: new FormControl(0,{ validators:[
-          Validators.required, 
-        ]}),
-         bathrooms: new FormControl(0,{ validators:[
-          Validators.required, 
-        ]}),
-        floor: new FormControl(0,{ validators:[
-          Validators.required, 
-        ]}),   
+      unitInfo: new FormGroup({
+        description: new FormControl('', {
+          validators: [Validators.required, Validators.pattern('[a-zA-Z ]*')],
+        }),
+        rooms: new FormControl(0, { validators: [Validators.required] }),
+        bathrooms: new FormControl(0, { validators: [Validators.required] }),
+        floor: new FormControl(0, { validators: [Validators.required] }),
       }),
 
-      address:new FormGroup({
-        city: new FormControl(""),
-        buildingNumber: new FormControl(0,{validators:[
-          Validators.required, 
-        ]}),
-       streetName: new FormControl("",{validators:[
-        Validators.required,
-        Validators.pattern("^[a-z0-9\s]+") 
-      ]}),      
-      })
+      address: new FormGroup({
+        city: new FormControl(''),
+        buildingNumber: new FormControl(0, {
+          validators: [Validators.required],
+        }),
+        streetName: new FormControl('', {
+          validators: [Validators.required, Validators.pattern('^[a-z0-9s]+')],
+        }),
+      }),
+    }),
+  });
 
+  cityName: string = '';
+  cityId: any;
 
-    })
-  })
+  getCityId(event: any) {
+    // console.log(event.target);
+    // console.log(event.target.value);
 
-  cityName:string=""
-  cityId:any
+    this.cityName = event.target.value;
+    console.log(this.cityName);
+    // console.log(name);
 
-  
-
-  getCityId(name:any){
-    this.cityName=name.label
-    console.log(this.cityName)
-
-   for (let i=0; i<this.cityArray.length;i++){
-    
-    if(this.cityName==this.cityArray[i].name){
-       this.cityId=this.cityArray[i]._id
+    for (let i = 0; i < this.cityArray.length; i++) {
+      if (this.cityName == this.cityArray[i].name) {
+        this.cityId = this.cityArray[i]._id;
+      }
     }
-
-   }
-   console.log(this.cityId)
+    console.log(this.cityId);
   }
 
- 
+  save() {
+    this.myform.controls.unit.value.cityId = this.cityId;
+    console.log(this.cityId);
+    console.log(this.myform.controls.unit.value.cityId);
 
+    this.myform.controls.unit.value.landlordId = this.authService.getUser()._id;
 
-  save(){
-        
-
-
-    this.myform.controls.unit.value.cityId=this.cityId,
-    this.myform.controls.unit.value.landlordId="41da0b86ae0fef497d2326cd"
-  
-
-    console.log(this.myform.controls.unit.value)
-    this.data.addUnit(this.myform.controls.unit.value).subscribe(a=>{
+    console.log(this.myform.controls.unit.value);
+    this.data.addUnit(this.myform.controls.unit.value).subscribe((a) => {
       console.log(a);
-    
-
-      
-      
-    })
-
-
-    
-
-
+    });
   }
 
-  cityArray: City[]=[]
+  cityArray: City[] = [];
   ngOnInit(): void {
     this.data.getAllCities(`/cities`).subscribe((res: any) => {
       this.cityArray = res;
       console.log(this.cityArray);
     });
-
-    
-    }
   }
+}
 
 // }

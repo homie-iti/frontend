7d5f61f634967e4cded2d671668aa05014b6e2bc
Radkillua <radwahmhus@gmail.com>
2022-08-12 19:09:20 +0200

    new password component linking to backend route

--- src/app/user/registration pages/newpassword/newpassword.component.ts
+++ src/app/user/registration pages/newpassword/newpassword.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { GetdataService } from 'src/app/service/getdata.service';
 
 @Component({
   selector: 'app-newpassword',
@@ -7,7 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewpasswordComponent implements OnInit {
 
-  constructor() { }
+  constructor(private data:GetdataService) { }
+
+  myform=new FormGroup({
+    password:new FormControl(null,{validators:[
+      Validators.required,
+      Validators.pattern("")
+    ]}),
+
+    resetLink:new FormControl(null,{validators:[
+      Validators.required,
+    ]})
+  })
+
+  send(){
+    console.log(this.myform.value)
+    this.data.resetPassword(this.myform.value).subscribe((a) => {
+      if(a)
+      console.log('upadated', a);
+    });
+  }
 
   ngOnInit(): void {
   }

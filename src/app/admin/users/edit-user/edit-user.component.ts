c4d4b0bf617c9a896d5b31021a2d8178930d6c8e
maryamragab <mariam.relshafei@yahoo.com>
2022-08-12 05:34:34 +0200

    adding default style for admin module in admin component

--- src/app/admin/users/edit-user/edit-user.component.ts
+++ src/app/admin/users/edit-user/edit-user.component.ts
@@ -7,7 +7,7 @@ import { User } from 'src/app/_models/user';
 @Component({
   selector: 'app-edit-user',
   templateUrl: './edit-user.component.html',
-  styleUrls: ['./edit-user.component.css'],
+  styleUrls: ['../../admin.component.css'],
 })
 export class EditUserComponent implements OnInit {
   constructor(
